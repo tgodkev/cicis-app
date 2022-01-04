e88553fb4a419b23ffcef9ce03c4b77e7aca6f38
kevin knight <tgodkev@gmail.com>
2022-01-04 00:35:17 -0500

    added icon and style changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-
+import Cheese from './cheese.png'
 
 function Header(){
     return(
         <div className='header'>
             <h1>Cici's SharkCoochie.</h1>
+            <img src={Cheese} alt="" />
         </div>
     )
 }
